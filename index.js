98e9eeea86dce91b24bc460665cbbda5d2712dc9
Malek Haj-Hussein <malekadair@gmail.com>
2019-10-10 19:37:35 -0500

    created repository files. .html .css .js

---
+++ index.js
@@ -0,0 +1,30 @@
+const options = {
+	headers: new Headers({
+		"Accept": application/vnd.github.v3+json})
+}
+
+
+
+// var settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://api.github.com/users/codybarr/repos",
+// 	"method": "GET",
+// 	"headers": {
+// 	  "Accept": "",
+// 	  "User-Agent": "PostmanRuntime/7.17.1",
+// 	  "Cache-Control": "no-cache",
+// 	  "Postman-Token": "afe00302-99c0-4977-b880-b9762a8c2b16,635de28e-2b20-474a-b265-ee1beb534b71",
+// 	  "Host": "api.github.com",
+// 	  "Accept-Encoding": "gzip, deflate",
+// 	  "Connection": "keep-alive",
+// 	  "cache-control": "no-cache"
+// 	}
+//   }
+  
+//   $.ajax(settings).done(function (response) {
+// 	console.log(response);
+//   });
+
+
+fetch(url, options)
