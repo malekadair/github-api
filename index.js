847ef9c1127d4a19fd0735ddea23d08f59e6029e
Malek Haj-Hussein <malekadair@gmail.com>
2019-10-10 20:11:46 -0500

    completed assignment.

--- index.js
+++ index.js
@@ -1,30 +1,64 @@
+const base_url = `https://api.github.com`
+
 const options = {
 	headers: new Headers({
-		"Accept": application/vnd.github.v3+json})
+		Accept: 'application/vnd.github.v3+json'
+	})
 }
 
+function getRepoHTML(repos) {
+	return repos.map((repo, index) => {
+		return `<li><a href=${repo.url} target="_balnk" rel="noopener">${repo.name}</a></li>`
+	})
+}
 
+function getUserRepos(username) {
+	const url = `${base_url}/users/${username}/repos`
 
-// var settings = {
-// 	"async": true,
-// 	"crossDomain": true,
-// 	"url": "https://api.github.com/users/codybarr/repos",
-// 	"method": "GET",
-// 	"headers": {
-// 	  "Accept": "",
-// 	  "User-Agent": "PostmanRuntime/7.17.1",
-// 	  "Cache-Control": "no-cache",
-// 	  "Postman-Token": "afe00302-99c0-4977-b880-b9762a8c2b16,635de28e-2b20-474a-b265-ee1beb534b71",
-// 	  "Host": "api.github.com",
-// 	  "Accept-Encoding": "gzip, deflate",
-// 	  "Connection": "keep-alive",
-// 	  "cache-control": "no-cache"
-// 	}
-//   }
-  
-//   $.ajax(settings).done(function (response) {
-// 	console.log(response);
-//   });
+	fetch(url, options)
+		.then(response => {
+			// the new code starts here
+			if (response.ok) {
+				return response.json()
+			}
+			throw new Error(response.statusText)
+		})
+		.then(repos => {
+			console.log(repos) // [{ name, url }]
 
+			// clear the error
+			$('#js-error-message').text('')
 
-fetch(url, options)
+			// if repos is empty
+			if (repos.length < 1) {
+				console.log('empty repo')
+				$('#js-error-message').text(`That user has no repos!`)
+			} else {
+				const reposHTML = getRepoHTML(repos)
+				
+				// unhide results section & render HTML
+				$('#results').show()
+                $('#results-list').html(reposHTML)
+			}
+		})
+		.catch(err => {
+			console.log('err!')
+			$('#js-error-message').text(`Something went wrong: ${err.message}`)
+		})
+}
+
+function handleSubmit() {
+	$('#search-form').on('submit', e => {
+		e.preventDefault()
+
+		// hide #results-list
+		$('#results').hide()
+
+		const username = $('#search').val()
+		const repos = getUserRepos(username)
+	})
+}
+
+$(() => {
+	handleSubmit()
+})
